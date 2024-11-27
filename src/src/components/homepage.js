e6108dfc2e338e27385462a0364ab888a207698e
DAdam03 <domokosadam03@gmail.com>
2024-11-27 14:27:47 +0100

    Upload build error

---
+++ src/src/components/homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TitleHero from './titlehero';
+
+const HomePage = () => (
+    <div>
+        <TitleHero src="./_assets/flexibility.jpg" alt="title hero image" title="Ez egy home page!"></TitleHero>
+        
+        
+    </div>
+);
+
+export default HomePage;
